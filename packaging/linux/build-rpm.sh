f341acb983bf2e8c009eef0e07af5d0aa52a37fc
Nguyen Van Tam <tamnvhustcc@gmail.com>
2024-08-30 16:13:54 +0700

    feature: add rpm

--- packaging/linux/build-rpm.sh
+++ packaging/linux/build-rpm.sh
@@ -14,22 +14,22 @@ ARCH="x86_64"
 
 # Create directory structure for the RPM package
 mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
-mkdir -p ~/rpmbuild/BUILDROOT/${PKG_NAME}_${VERSION}_${RELEASE}.${ARCH}
+mkdir -p ~/rpmbuild/BUILDROOT/${PKG_NAME}-${VERSION}-${RELEASE}.${ARCH}
 
 # Create the directory structure in BUILDROOT
-mkdir -p ~/rpmbuild/BUILDROOT/${PKG_NAME}_${VERSION}_${RELEASE}.${ARCH}/opt/${PKG_NAME}
-mkdir -p ~/rpmbuild/BUILDROOT/${PKG_NAME}_${VERSION}_${RELEASE}.${ARCH}/usr/share/applications
-mkdir -p ~/rpmbuild/BUILDROOT/${PKG_NAME}_${VERSION}_${RELEASE}.${ARCH}/usr/share/icons/hicolor/256x256/apps
-mkdir -p ~/rpmbuild/BUILDROOT/${PKG_NAME}_${VERSION}_${RELEASE}.${ARCH}/usr/bin
+mkdir -p ~/rpmbuild/BUILDROOT/${PKG_NAME}-${VERSION}-${RELEASE}.${ARCH}/opt/${PKG_NAME}
+mkdir -p ~/rpmbuild/BUILDROOT/${PKG_NAME}-${VERSION}-${RELEASE}.${ARCH}/usr/share/applications
+mkdir -p ~/rpmbuild/BUILDROOT/${PKG_NAME}-${VERSION}-${RELEASE}.${ARCH}/usr/share/icons/hicolor/256x256/apps
+mkdir -p ~/rpmbuild/BUILDROOT/${PKG_NAME}-${VERSION}-${RELEASE}.${ARCH}/usr/bin
 
 # Copy the entire application directory
-cp -R dist/${DISPLAY_NAME}/* ~/rpmbuild/BUILDROOT/${PKG_NAME}_${VERSION}_${RELEASE}.${ARCH}/opt/${PKG_NAME}/
+cp -R dist/${DISPLAY_NAME}/* ~/rpmbuild/BUILDROOT/${PKG_NAME}-${VERSION}-${RELEASE}.${ARCH}/opt/${PKG_NAME}/
 
 # Create a soft link in /usr/bin
-ln -s /opt/${PKG_NAME}/${DISPLAY_NAME} ~/rpmbuild/BUILDROOT/${PKG_NAME}_${VERSION}_${RELEASE}.${ARCH}/usr/bin/${PKG_NAME}
+ln -s /opt/${PKG_NAME}/${DISPLAY_NAME} ~/rpmbuild/BUILDROOT/${PKG_NAME}-${VERSION}-${RELEASE}.${ARCH}/usr/bin/${PKG_NAME}
 
 # Create the .desktop file
-cat << EOF > ~/rpmbuild/BUILDROOT/${PKG_NAME}_${VERSION}_${RELEASE}.${ARCH}/usr/share/applications/${PKG_NAME}.desktop
+cat << EOF > ~/rpmbuild/BUILDROOT/${PKG_NAME}-${VERSION}-${RELEASE}.${ARCH}/usr/share/applications/${PKG_NAME}.desktop
 [Desktop Entry]
 Name=${DISPLAY_NAME}
 Exec=/usr/bin/${PKG_NAME}
@@ -39,7 +39,7 @@ Categories=Utility;
 EOF
 
 # Copy the icon
-cp ../../screenbolt/resources/icons/screenbolt.png ~/rpmbuild/BUILDROOT/${PKG_NAME}_${VERSION}_${RELEASE}.${ARCH}/usr/share/icons/hicolor/256x256/apps/${PKG_NAME}.png
+cp ../../screenbolt/resources/icons/screenbolt.png ~/rpmbuild/BUILDROOT/${PKG_NAME}-${VERSION}-${RELEASE}.${ARCH}/usr/share/icons/hicolor/256x256/apps/${PKG_NAME}.png
 
 # Create the SPEC file
 cat << EOF > ~/rpmbuild/SPECS/${PKG_NAME}.spec
@@ -78,10 +78,10 @@ EOF
 rpmbuild -bb ~/rpmbuild/SPECS/${PKG_NAME}.spec
 
 # Move the RPM to the current directory
-RPM_FILE=~/rpmbuild/RPMS/${ARCH}/${PKG_NAME}_${VERSION}_${RELEASE}.${ARCH}.rpm
+RPM_FILE=~/rpmbuild/RPMS/${ARCH}/${PKG_NAME}-${VERSION}-${RELEASE}.${ARCH}.rpm
 if [ -f "$RPM_FILE" ]; then
     mv "$RPM_FILE" .
-    echo "RPM package created and moved: ${PKG_NAME}_${VERSION}_${RELEASE}.${ARCH}.rpm"
+    echo "RPM package created and moved: ${PKG_NAME}-${VERSION}-${RELEASE}.${ARCH}.rpm"
 else
     echo "Error: RPM file not found at $RPM_FILE"
     exit 1
